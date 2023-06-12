cb1fd4544e1eb5e444028bccfc71d6753b79494e
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-12 19:02:54 +0300

    add

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,15 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+
+        try{
+            int number = Integer.parseInt(str);
+            return number > 0;
+        }  catch (NumberFormatException ex) {
+            return false;
+        }
     }
 }
